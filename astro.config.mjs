cff5372715b74c7c02d39d008de3354af511456f
Britnell <tommy.britnell@gmail.com>
2024-02-07 15:37:40 +0100

    /portfolio?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://britnell.github.io",
-  base: "/who",
+  base: "/portfolio",
 });

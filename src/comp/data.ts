66c8e63d68e0e38007bfad251d9234bc1dd716b3
Britnell <tommy.britnell@gmail.com>
2024-02-14 14:31:35 +0100

    page comp + companies section

---
+++ src/comp/data.ts
@@ -0,0 +1,32 @@
+export const companies = [
+  {
+    name: "flyp",
+    url: "https://www.flyp.co/",
+    img: "/logos/flyp.png",
+  },
+  {
+    name: "Made By On",
+    url: "https://madebyon.com/",
+    img: "/logos/on.png",
+  },
+  {
+    name: "Nio",
+    url: "https://www.nio.com/",
+    img: "/logos/nio.png",
+  },
+  {
+    name: "Design Affairs",
+    url: "https://www.designaffairs.com/",
+    img: "/logos/desaff.png",
+  },
+  {
+    name: "The Makery",
+    url: "https://www.makery.de/",
+    img: "/logos/makery.png",
+  },
+  {
+    name: "Engineers without Borders UK",
+    url: "https://www.ewb-uk.org/",
+    img: "/logos/ewb.png",
+  },
+];
